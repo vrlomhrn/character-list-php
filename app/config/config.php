813123c047c233c9de3f9a32ec46093171714035
vrlomhrn <vmahrian@gmail.com>
2023-03-09 17:39:51 +0700

    update files

--- app/config/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-define("BASEURL", "http://localhost/phpmvc/public");
-
-
-// Database
-define("DB_HOST", "localhost");
-define("DB_USER", "root");
-define("DB_PASSWORD", "");
-define("DB_NAME", "phpmvc");
